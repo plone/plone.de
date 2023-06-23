c2d012c1f4e1226cad3414463be3e46f3c0e7a4f
Jonas Piterek <piterek@kitconcept.com>
2023-06-23 10:23:53 +0200

    Update slate headings & improve separator

---
+++ frontend/src/addons/volto-plonede/src/customizations/@kitconcept/volto-separator-block/components/View.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import cx from 'classnames';
+
+const SeparatorView = (props) => {
+  //eslint-disable-next-line
+  const { className, data } = props;
+  return (
+    <div className="block separator">
+      <div className={cx('inner-wrapper', className)}>
+        <div className="line" />
+      </div>
+    </div>
+  );
+};
+
+export default SeparatorView;
